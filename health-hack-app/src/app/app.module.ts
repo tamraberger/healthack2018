37e11443874857f6c13c9a18071e99116f90f4c6
CORPORATE\spg1002 <sean.gibbons@daugherty.com>
2018-04-07 12:39:52 -0500

    Added routing

--- health-hack-app/src/app/app.module.ts
+++ health-hack-app/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HomePageComponent } from './home-page/home-page.component';
@@ -12,6 +12,24 @@ import { ProfilePageComponent } from './profile-page/profile-page.component';
 import { FriendsAndFamiliyViewComponent } from './friends-and-familiy-view/friends-and-familiy-view.component';
 import { Bootstrap } from 'bootstrap';
 
+const appRoutes: Routes = [
+  { path: 'app-home-page',
+    component: HomePageComponent
+  },
+  { path: 'app-all-learning-page',
+    component: AllLearningPageComponent
+  },
+  { path: 'app-profile-page',
+    component: ProfilePageComponent
+  },
+  { path: 'app-quiz-page',
+    component: QuizPageComponent
+  },
+  { path: 'app-specific-learning-page',
+    component: SpecificLearningPageComponent
+  },
+  { path: '', redirectTo: '/app-home-page', pathMatch: 'full' }
+];
 
 @NgModule({
   declarations: [
@@ -25,6 +43,7 @@ import { Bootstrap } from 'bootstrap';
     FriendsAndFamiliyViewComponent
   ],
   imports: [
+    RouterModule.forRoot(appRoutes),
     BrowserModule
   ],
   providers: [],
